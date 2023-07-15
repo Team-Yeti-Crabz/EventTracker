e2f473361cf73af2e8a8b445de9ba7686a7a455d
Clayton Stewart <claytons1212@gmail.com>
2023-07-15 15:31:54 -0600

    setting up home controllers for home page request

---
+++ server/routes/homeRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+//add in any controllers
+
+const router = express.Router();
+
+//add routers as needed
+/*
+received request from hope page with username in body
+pull user document from database
+request show info from seatgeek based on preferences
+sending back event data to client
+Expect request:
+    { 
+    Email: String,
+    Location: {
+        City: String,
+        State: String,
+        }
+    Artists: [Artist1, Artist2, Artist3]
+    Genres: [Genre1, Genre2, Genre3]
+    }
+    */
+
+router.get('/', userController.getUserInfo, (req, res) => {
+  return res.status(200).json(res.locals.userInfo);
+});
