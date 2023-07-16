ba33d56a0e2b0e0c4af4a8ec5f982be17bd8a54d
Rylie <ryles@MacBook-Pro-4.ht.home>
2023-07-16 16:41:26 -0700

    updated package.json for mac

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -13,10 +13,12 @@ router.get('/', authController.initializeAuth, (req, res) => {
 // user is redirected here from spotify after entering their credentials
 // if the user's credentials were authenticated, make get request to obtain refresh and access tokens from spotify
 // TODO: if succesful, send the logged res.locals info to server to store for duration of user session
-router.get('/callback', authController.checkState, authController.getTokens, (req, res) => {
-    console.log('acces token: ', res.locals.accessToken);
-    console.log('refresh token: ', res.locals.refreshToken);
-    console.log('options to pass into spotify api requests: ', res.locals.options);
+// authController.checkState, authController.getTokens, 
+router.get('/callback', (req, res) => {
+    console.log('received redirect from spotify');
+    // console.log('acces token: ', res.locals.accessToken);
+    // console.log('refresh token: ', res.locals.refreshToken);
+    // console.log('options to pass into spotify api requests: ', res.locals.options);
     return res.sendStatus(200);
 
 });
