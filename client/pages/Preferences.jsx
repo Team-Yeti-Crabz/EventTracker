2a86b4f8b5c3a86e31ba780ce62adee525d5c3c9
Halia Haynes <haliahaynes@Halias-Mac.local>
2023-07-16 15:01:51 -1000

    updated

--- client/pages/Preferences.jsx
+++ client/pages/Preferences.jsx
@@ -7,10 +7,7 @@ export default function Preference() {
   const location = useLocation();
   const [email, setEmail] = useState('');
   const [userData, setUserData] = useState({});
-  //   const [location, setLocation] = useState({});
-  const [newArtist, setNewArtist] = useState('');
   const [currArtists, setCurrArtists] = useState([]);
-  const [newGenre, setNewGenre] = useState('');
   const [currGenres, setCurrGenres] = useState([]);
 
   useEffect(() => {
@@ -29,6 +26,7 @@ export default function Preference() {
           );
           const data = await response.json();
           // {
+          // email
           // city:
           // state:
           // artists:[1,2,3]
@@ -43,14 +41,29 @@ export default function Preference() {
     }
   }, [location]);
 
+  //changing state's state
+  const handleChangeCity = (e) => {
+    const newCity = e.target.value;
+    setUserData((curr) => ({
+      ...curr,
+      city: newCity,
+    }));
+  };
+  //changing city's state
+  const handleChangeState = (e) => {
+    const newState = e.target.value;
+    setUserData((curr) => ({
+      ...curr,
+      state: newState,
+    }));
+  };
   //sending PATCH request with updated state
   const handleLocation = async (e) => {
     e.preventDefault();
     try {
       const toUpdate = {
         email: email,
-        city: userData.city,
-        state: userData.state,
+        location: { city: userData.city, state: userData.state },
       };
       await fetch(`/api/preferences?email=${encodeURIComponent(email)}`, {
         method: 'PATCH',
@@ -61,74 +74,49 @@ export default function Preference() {
       console.log(err);
     }
   };
-  //changing state
-  const handleChangeCity = (e) => {
-    const newCity = e.target.value;
-    setNewData((curr) => ({
-      ...curr,
-      city: newCity,
-    }));
+  //changing artistArr's state
+  const handleChangeAddArtist = (e) => {
+    const newArtist = e.target.value;
+    setCurrArtists((curr) => [...curr, newArtist]);
   };
-  //changing state
-  const handleChangeState = (e) => {
-    const newState = e.target.value;
-    setNewData((curr) => ({
-      ...curr,
-      state: newState,
-    }));
-  };
-
+  //sending a PATCH request with updated artists array
   const handleAddArtist = async (e) => {
     e.preventDefault();
     try {
       const addInfo = {
         email: email,
-        artist: newArtist,
+        artists: currArtists,
       };
       await fetch(`/api/preferences?email=${encodeURIComponent(email)}`, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(addInfo),
       });
-      setCurrArtists([...currArtists, newArtist]);
-      setNewArtist('');
     } catch (err) {
       console.log(err);
     }
   };
-
-  console.log('userData: ', userData);
-
-  const handleChangeAddArtist = (e) => {
-    const newArtist = e.target.value;
-    setCurrArtists((curr) => [...curr, newArtist]);
+  //changing genre's state
+  const handleChangeAddGenre = (e) => {
+    const newGenre = e.target.value;
+    setCurrGenres((curr) => [...curr, newGenre]);
   };
-
+  //sending a PATCH request with updated genre array
   const handleAddGenre = async (e) => {
     e.preventDefault();
     try {
       const addInfo = {
         email: email,
-        artist: newArtist,
-        genre: newGenre,
+        genres: currGenres,
       };
-      await fetch(`/api/preferences?email=${encodeURIComponent(userEmail)}`, {
+      await fetch(`/api/preferences?email=${encodeURIComponent(email)}`, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(addInfo),
       });
-      setCurrArtists([...currArtists, newArtist]);
-      setCurrGenres([...currGenres, newGenre]);
-      setNewArtist('');
-      setNewGenre('');
     } catch (err) {
       console.log(err);
     }
-  };
-
-  const handleChangeAddGenre = (e) => {
-    const newGenre = e.target.value;
-    setCurrGenres((curr) => [...curr, newGenre]);
   };
 
   return (
@@ -203,11 +191,19 @@ export default function Preference() {
       <div className="current">
         <div className="currentArtists">
           <h2>Current Artists Tracked:</h2>
-          {/* <h4>{currentArtists}</h4> */}
+          <ul>
+            {currArtists.map((artist, i) => {
+              <li key={artist + i}>{artist}</li>;
+            })}
+          </ul>
         </div>
         <div className="currentGenres">
           <h2>Current Genres Tracked:</h2>
-          {/* <h4>{currentArtists}</h4> */}
+          <ul>
+            {currGenres.map((genre, i) => {
+              <li key={genre + i}>{genre}</li>;
+            })}
+          </ul>
         </div>
       </div>
     </div>
